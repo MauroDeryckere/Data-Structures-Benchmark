94c60513ce55bcfbe503d7988491a225aecafdc0
Mauro Deryckere <mauro.deryckere@gmail.com>
2025-09-27 18:53:36 +0200

    setup for results folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,52 @@
 #include <iostream>
+#include <string>
+
+#include <filesystem>
+#include <fstream>
+
+[[nodiscard]] static std::string GetCompilerInfo() noexcept
+{
+	#if defined(_MSC_VER)
+		return "MSVC " + std::to_string(_MSC_VER) + " (full: " + std::to_string(_MSC_FULL_VER) + ")";
+	#elif defined(__clang__)
+		return "Clang " + std::to_string(__clang_major__) + "." +
+			std::to_string(__clang_minor__) + "." +
+			std::to_string(__clang_patchlevel__);
+	#elif defined(__GNUC__)
+		return "GCC " + std::to_string(__GNUC__) + "." +
+			std::to_string(__GNUC_MINOR__) + "." +
+			std::to_string(__GNUC_PATCHLEVEL__);
+	#else
+		return "Unknown compiler";
+	#endif
+}
 
 int main()
 {
-	#if defined(_MSC_VER)
-	    std::cout << "MSVC "
-	        << _MSC_VER << " (full: " << _MSC_FULL_VER << ")\n";
-	#elif defined(__clang__)
-	    std::cout << "Clang "
-	        << __clang_major__ << "."
-	        << __clang_minor__ << "."
-	        << __clang_patchlevel__ << "\n";
-	#elif defined(__GNUC__)
-	    std::cout << "GCC "
-	        << __GNUC__ << "."
-	        << __GNUC_MINOR__ << "."
-	        << __GNUC_PATCHLEVEL__ << "\n";
-	#else
-	    std::cout << "Unknown compiler\n";
-	#endif
+	std::string const info{ GetCompilerInfo() };
+	std::cout << info << "\n";
+
+	std::string safeName{ info };
+	for (char& c : safeName) 
+	{
+		if (c == ' ' || c == '(' || c == ')' || c == ':') c = '_';
+	}
+
+	std::filesystem::path const resultsDir{ std::filesystem::path(PROJECT_RESULTS_DIR) };
+	std::filesystem::create_directories(resultsDir);
+	std::filesystem::path const filePath{ resultsDir / ("compiler_info_" + safeName + ".txt") };
+
+	std::cout << "Writing results to: " << filePath << "\n";
+
+	std::ofstream out(filePath);
+	if (out.is_open())
+	{
+		out << info << "\n";
+	}
+	else {
+		std::cerr << "Error: could not write to " << filePath << "\n";
+		return 1;
+	}
 
 	return 0;
 }
